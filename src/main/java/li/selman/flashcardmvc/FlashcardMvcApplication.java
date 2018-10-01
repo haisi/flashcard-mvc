0c644b0fbc43c40c14d3d3726777aaa70a0e5495
Hasan Kara <hasan.kara@students.fhnw.ch>
2018-10-01 13:16:28 +0200

    Init

---
+++ src/main/java/li/selman/flashcardmvc/FlashcardMvcApplication.java
@@ -0,0 +1,12 @@
+package li.selman.flashcardmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlashcardMvcApplication.class, args);
+    }
+}
