aa87e7d056c261295ac5de81c7e4e1aa5f6ec058
Hasan Kara <hasan.kara@students.fhnw.ch>
2018-10-01 14:00:51 +0200

    Save sample entities into MongoDB

--- src/main/java/li/selman/flashcardmvc/FlashcardMvcApplication.java
+++ src/main/java/li/selman/flashcardmvc/FlashcardMvcApplication.java
@@ -1,12 +1,28 @@
 package li.selman.flashcardmvc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FlashcardMvcApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FlashcardMvcApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner run(QuestionnaireRepository repository) {
+        return args -> {
+            repository.deleteAll();
+
+            repository.save(Questionnaire.of("Test 1", "Desc 1"));
+            repository.save(Questionnaire.of("Test 2", "Desc 2"));
+            repository.save(Questionnaire.of("Test 3", "Desc 3"));
+
+            repository.findAll().forEach(System.out::println);
+        };
     }
 }
