5eb01b4bbe645f982f22a192a2287daafb97c4f8
Hasan Kara <hasan.kara@students.fhnw.ch>
2018-10-14 14:42:21 +0200

    Add bean-validation to Questionnaire

--- src/main/java/li/selman/flashcardmvc/Questionnaire.java
+++ src/main/java/li/selman/flashcardmvc/Questionnaire.java
@@ -2,12 +2,17 @@ package li.selman.flashcardmvc;
 
 import org.springframework.data.annotation.Id;
 
+import javax.validation.constraints.Size;
+
 public class Questionnaire {
 
     @Id
     public final String id;
 
+    @Size(min = 2, max = 30)
     private String title;
+
+    @Size(min = 10, max = 50)
     private String description;
 
     static Questionnaire of(String title, String description) {
